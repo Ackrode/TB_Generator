6d6f3a5d8621802a55f8671cb9f5ccbbde660791
ChuchoLT <93542544+ChuchoLT@users.noreply.github.com>
2021-11-09 12:57:17 -0600

    cambio

--- design.sv
+++ design.sv
@@ -1,11 +1,23 @@
 
-module and_gate(in_a, b,we,ew, a, c, w, Data);
-   input a, c, w;
-   input wire [1:0] Data;
-    output c,dt;
-   output wire [2:0] out_c1;
-  inout tc;
+module  sevensegment_CC(data, data_segment);
+  input wire [3:0] data,data1;
+  output reg [6:0] data_segment;
   
-  assign c= a & b;
-  
+   always @*
+    begin 
+      case (data) 
+        4'b0000:  data_segment=7'b1111110;
+        4'b0001:  data_segment=7'b0110000;
+        4'b0010:  data_segment=7'b1101101;
+        4'b0011:  data_segment=7'b1111001;
+        4'b0100:  data_segment=7'b0110011;
+        4'b0101:  data_segment=7'b1011011;
+        4'b0110:  data_segment=7'b1011111;
+        4'b0111:  data_segment=7'b1110000;
+        4'b1000:  data_segment=7'b1111111;
+        4'b1001:  data_segment=7'b1111011;
+        default:  data_segment=7'b1110111;
+             endcase
+        end
+ 
 endmodule
