4f892a8aae2f122c1649c71aeda6abc87afe7b66
Ackrode <92236579+Ackrode@users.noreply.github.com>
2021-11-09 08:09:16 -0700

    error <8 instead >8

--- stimulus.py
+++ stimulus.py
@@ -1,19 +1,19 @@
 def stimulus(inputs, numero):
-    if numero <8:  
+    if numero >8:  
         return ValueError("Number of inputs needs to be less than 8")
-        
-    strings=['']
-    for i in range(0,2**numero):
-        for name in inputs.get('name'):
-            if name==inputs.get('name')[0]:
-                strings[i]='{'+name+','
-            elif name==inputs.get('name')[-1]:
-                strings[i]+=' '+name
-            else:
-                strings[i]+=name+','   
-        strings[i]=strings[i]+'}='+str(numero)+"b'"+str(format(i,'0'+str(numero)+'b')+'; #1')
-        strings.append(strings[i])
-    return strings
+    else:   
+        strings=['']
+        for i in range(0,2**numero):
+            for name in inputs.get('name'):
+                if name==inputs.get('name')[0]:
+                    strings[i]='{'+name+','
+                elif name==inputs.get('name')[-1]:
+                    strings[i]+=' '+name
+                else:
+                    strings[i]+=name+','   
+            strings[i]=strings[i]+'}='+str(numero)+"b'"+str(format(i,'0'+str(numero)+'b')+'; #1')
+            strings.append(strings[i])
+        return strings  
     #with open('stimulus.txt', 'w') as filehandle:
     #    for listitem in strings[0:-1]:
     #            filehandle.write('%s\n' % listitem)  #write list with every element in a new line
